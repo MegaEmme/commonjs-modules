dc996032066f08853b969d8cc167bccec6a5ea66
MegaEmme <mikmusco@gmail.com>
2025-03-24 13:51:13 +0100

    feat: create names.js

---
+++ names.js
@@ -0,0 +1,10 @@
+function fullName (firstName, lastName){
+    return {firstName,lastName}
+}
+
+// const myFirstName = 'Michelangelo';
+// const myLastName = 'Musco';
+
+// console.log(fullName(myFirstName,myLastName));
+
+module.exports = fullName;
